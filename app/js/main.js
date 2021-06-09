929870a59f831a96acdbccaf5b349d1351296e83
IvanVerushkin <tenekrut30@gmail.com>
2021-06-09 19:06:36 -0300

    do index2

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+$('.slider__body').slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 600,
+    speed: 2500,
+    responsive:[
+       {
+          breakpoint: 1025,
+          settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true,
+         }
+       },
+       {
+         breakpoint: 769,
+          settings: {
+          slidesToShow: 1,
+          arrows: false,
+          dots: true
+         }
+       }
+    ]
+});
